4e9637040587438603958fc41342205837255959
strnad17@gmail.com <>
2017-06-27 19:14:50 +0200

    adding a column

--- AW2014/Tables/dbo.Table_1.sql
+++ AW2014/Tables/dbo.Table_1.sql
@@ -3,7 +3,8 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table_1] (
-		[test1]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[test1]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[test2]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table_1] SET (LOCK_ESCALATION = TABLE)
