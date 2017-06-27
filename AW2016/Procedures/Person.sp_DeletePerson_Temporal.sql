5e64679459e8034410ae9f9abd4b862070c9c03c
strnad17@gmail.com <>
2017-06-27 16:23:03 +0200

    All objects committed

---
+++ AW2016/Procedures/Person.sp_DeletePerson_Temporal.sql
@@ -0,0 +1,16 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+
+/*
+	Stored procedure that deletes row in [Person].[Person_Temporal]
+	and corresponding row in [HumanResources].[Employee_Temporal]
+*/
+CREATE PROCEDURE [Person].[sp_DeletePerson_Temporal]
+@BusinessEntityID INT
+AS
+
+DELETE FROM [HumanResources].[Employee_Temporal] WHERE [BusinessEntityID] = @BusinessEntityID;
+DELETE FROM [Person].[Person_Temporal] WHERE [BusinessEntityID] = @BusinessEntityID;
+
+GO
